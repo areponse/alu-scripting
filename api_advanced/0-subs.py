8b9f79825c8a4b0b56f38a2c885468c2295ea332
Reponse Ashimwe <reponseashime@gmail.com>
2024-02-28 11:20:43 +0200

    API Advanced

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -5,10 +5,14 @@
 
 import requests
 
+
 def number_of_subscribers(subreddit):
     """Returns the number of subscribers for a given subreddit."""
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'}
+    headers = \
+        {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7)\
+          AppleWebKit/537.36(KHTML, like Gecko) \
+         Chrome/90.0.4430.93 Safari/537.36'}
 
     response = requests.get(url, headers=headers, allow_redirects=False)
 
