4ccce5bf44f2c449a1898997c45ac81886b0df01
areponse <144727059+areponse@users.noreply.github.com>
2024-02-29 09:56:09 +0200

    Update 0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,26 +1,14 @@
 #!/usr/bin/python3
-"""
-0-subs
-"""
-
+""" 0-subs.py """
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers for a given subreddit."""
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = \
-        {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7)\
-          AppleWebKit/537.36(KHTML, like Gecko) \
-         Chrome/90.0.4430.93 Safari/537.36'}
-
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
         return response.json().get('data').get('subscribers')
     else:
         return 0
-
-
-if __name__ == '__main__':
-    print(number_of_subscribers("programming"))
